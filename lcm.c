d6dc5c9915fdc6841507e39676a3daabb0a87a46
arfy slowy <slowy.arfy@gmail.com>
2022-06-10 03:04:53 -0300

    adding lcm
    Signed-off-by: arfy slowy <slowy.arfy@gmail.com>

---
+++ lcm.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    int a, b;
+    printf("enter two number to find lcm: \n");
+    scanf("%d%d", &a, &b);
+    printf("LCM of %d and %d is %d", a, b, lcm(a, b));
+
+    return 0;
+}
