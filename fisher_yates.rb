b3426c08b681bac7881d27bc7e8bda25b4ec0808
arfy slowy <slowy.arfy@gmail.com>
2022-06-16 09:49:23 -0300

    adding fisher yates
    Signed-off-by: arfy slowy <slowy.arfy@gmail.com>

---
+++ fisher_yates.rb
@@ -0,0 +1,11 @@
+def fisher_yates_shuffle(array)
+    n = array.length
+    while n > 0
+        i = rand(n -= 1)
+        array[i], array[n] = array[n], array[i]
+    end
+    array
+end
+
+arr = [1, 2, 40, 30, 20, 15, 323, 12, 3, 4]
+puts fisher_yates_shuffle(arr)
