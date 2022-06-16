b048a1edbbe76ff541f412c2d38de31aa17a16cd
arfy slowy <slowy.arfy@gmail.com>
2022-06-15 23:07:23 -0300

    fixing testing spec
    Signed-off-by: arfy slowy <slowy.arfy@gmail.com>

--- .spec/ds/heap_spec.lua
+++ .spec/ds/heap_spec.lua
@@ -1,22 +1,21 @@
-describe("Heap", function
-    local shuffle = require("random.shuffle")
-    local list = {}
-    for index = 1, 100 do
-        list[index] = index
-    end
-
-    shuffle(list)
-    local heap = require("ds.heap")
-    local nums = heap.new()
-    for index = 1, #list do
-        nums:push(list[index])
-        assert.equal(index, nums:size())
-    end
-    for index = 1, #list do
-        assert.equal(#list - index + 1, nums:size())
-        assert.equal(index, nums:top())
-        local popped = nums:pop()
-        assert.equal(index, popped)
-    end
-    assert(nums:empty())
+describe("Heap", function()
+	local shuffle = require("random.shuffle")
+	local list = {}
+	for index = 1, 100 do
+		list[index] = index
+	end
+	shuffle(list)
+	local heap = require("ds.heap")
+	local nums = heap.new()
+	for index = 1, #list do
+		nums:push(list[index])
+		assert.equal(index, nums:size())
+	end
+	for index = 1, #list do
+		assert.equal(#list - index + 1, nums:size())
+		assert.equal(index, nums:top())
+		local popped = nums:pop()
+		assert.equal(index, popped)
+	end
+	assert(nums:empty())
 end)
