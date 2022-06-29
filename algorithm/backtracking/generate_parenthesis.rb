18f8c1107ba1e5c03adc399f7c4482c51a89605f
slowy07 <slowy.arfy@gmail.com>
2022-06-29 05:29:49 -0300

    adding generate_parenthesis
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ algorithm/backtracking/generate_parenthesis.rb
@@ -0,0 +1,19 @@
+def generate_parenthesis(n)
+    parenthesis = []
+    backtrack(parenthesis, "", 0, 0, n)
+    parenthesis
+end
+
+def backtrack(parenthesis, curr, open, close, max)
+    if curr.length == max * 2
+        parenthesis.push(curr)
+        return
+    end
+    backtrack(parenthesis, curr + '(', open + 1, close, max) if open < max
+    backtrack(parenthesis, curr + ')', open, close + 1, max) if close < open
+end
+
+n = 3
+print(generate_parenthesis(n))
+n = 1
+print(generate_parenthesis(n))
