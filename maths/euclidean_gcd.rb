9fc93a202483d51f617f670b345fdfab822a3fbb
slowy07 <slowy.arfy@gmail.com>
2022-11-05 05:59:19 +0700

    feat: adding greates common divisor
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ maths/euclidean_gcd.rb
@@ -0,0 +1,11 @@
+def euclidean_gcd(a, b)
+  while b != 0
+    t = b
+    b = a % b
+    a = t
+  end
+  a
+end
+
+puts 'GCD(3, 5) = '+ euclidean_gcd(3, 5).to_s
+puts 'GCD(6, 3) = '+ euclidean_gcd(6, 3).to_s
