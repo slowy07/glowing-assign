0b5ef3b645642fb4c299ac8b8d5ddc16abb0b58d
slowy07 <slowy.arfy@gmail.com>
2022-11-05 06:36:30 +0700

    feat: adding simpson rule and fixing linear regression
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- machine_learning/linear_regression.jl
+++ machine_learning/linear_regression.jl
@@ -1,39 +1,39 @@
 using Statistics
 
 function gradient_descent(
-  W::Matrix,
-  b,
-  X::Matrix,
-  Y::Matrix,
-  alpha::Float64,
-  num_iters::Int64,
+    W::Matrix,
+    b,
+    X::Matrix,
+    Y::Matrix,
+    alpha::Float64,
+    num_iters::Int64,
 )
-  m = length(Y)
-  J_history = []
-  for i in 1:num_iters
-    W = W - alpha .* (1 / m) * (X * (X'W .+ b - Y'))
-    b = b - alpha .* (1 / m) * sim((X'W .+ b - Y'))
-    push!(J_history, 1 / 2m * sum((X'W .+ b - Y') .^ 2))
-  end
-  return W, b, J_history
+    m = length(Y)
+    J_history = []
+    for i in 1:num_iters
+        W = W - alpha .* (1 / m) * (X * (X'W .+ b - Y'))
+        b = b - alpha .* (1 / m) * sum((X'W .+ b - Y'))
+        push!(J_history, 1 / 2m * sum((X'W .+ b - Y') .^ 2))
+    end
+    return W, b, J_history
 end
 
 function linear_regression(
-  X::Matrix,
-  Y::Matrix,
-  alpha::Float64,
-  num_iters::Int64,
+    X::Matrix,
+    Y::Matrix,
+    alpha::Float64,
+    num_iters::Int64,
 )
-  W = zeros(1, size(X, 1))
-  b = 0
-  W, b, J_history = gradient_descent(w, b, X, Y, alpha, num_iters)
-  return W, b, J_history
+    W = zeros(1, size(X, 1))
+    b = 0
+    W, b, J_history = gradient_descent(W, b, X, Y, alpha, num_iters)
+    return W, b, J_history
 end
 
 predict(X::Matrix, W::Matrix, b::Float64) = X'W .+ b;
 
-X = [1, 3, 2, 5, 7, 8, 8, 9, 10, 12]
-Y = [2, 4, 3, 6, 8, 9, 9, 10, 11, 13]
+X = [1, 3, 2, 2, 7, 8, 8, 11, 44, 12]
+Y = [2, 4, 2, 6, 8, 9, 9, 10, 23, 13]
 
 X = reshape(X, 1, length(X));
 Y = reshape(Y, 1, length(Y));
