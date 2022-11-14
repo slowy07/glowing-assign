72e88ec2e6320a72a0a009ff6e67ce4a70a93164
slowy07 <slowy.arfy@gmail.com>
2022-11-14 21:48:35 +0700

    chore: finished quartile
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- quartile.c
+++ quartile.c
@@ -18,8 +18,22 @@ int main() {
   for (i = 0; i< n; i++){
     for (j = 0; j < n; j++) {
       if (a[i] < a[j]) {
-
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
       }
     }
   }
+  for (i = 0; i < n; i++) {
+    printf("\n%d", a[i]);
+  }
+
+  q1 = a[n / 4];
+  printf("\nfirst quartile: %f", q1);
+  q3 = a[(3 * n) / 4];
+  printf("\nthird quartile: %f", q3);
+  iqr = q3 - q1;
+  printf("\ninteger quartile range: %f", iqr);
+
+  return 0;
 }
