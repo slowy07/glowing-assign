5986b8acf21a147487016ffd5f4786976b79b1ab
slowy07 <slowy.arfy@gmail.com>
2023-03-10 07:33:25 +0700

    fix: fixing binary to decimal
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- conversion/binary_to_decimal.c
+++ conversion/binary_to_decimal.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int remainder, number = 0; decimal_number = 0, temp = 1;
-    printf("\n  enter binary number: ");
-    scanf("%d", &number);
+  int remainder, number = 0, decimal_number = 0, temp = 1;
+  printf("\n  enter binary number: ");
+  scanf("%d", &number);
 
-    while (number > 0) {
-        remainder = number % 10;
-        number = number / 10;
-        decimal_number += remainder * temp;
+  while (number > 0) {
+    remainder = number % 10;
+    number = number / 10;
+    decimal_number += remainder * temp;
 
-        temp = temp * 2;
-    }
+    temp = temp * 2;
+  }
 
-    printf("%d\n", decimal_number);
+  printf("%d\n", decimal_number);
 }
